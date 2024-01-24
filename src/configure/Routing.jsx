e1f4213d13c55e7d78bfe7f5553ec78c0b7b864a
ChiragSethi-1153 <chirag.1153@zenmonk.tech>
2024-01-24 10:10:58 +0530

    commit

--- src/configure/Routing.jsx
+++ src/configure/Routing.jsx
@@ -1,35 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Home, UserSignup, UserLogin} from "../pages";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import UserSignup from "../pages/UserSignup";
+import UserLogin from "../pages/UserLogin";
 
 const Routing = () => {
   return (
     <Router>
-    <Route exact path="/" element={Home} />
-    <Route path="/signup" component={UserSignup} />
-        <Route path="/login" component={UserLogin} />
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route path="/signup" Component={UserSignup} />
+        <Route path="/login" Component={UserLogin} />
+      </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default Routing
-
-// function BasicExample() {
-//   return (
-//     <Router history={History} >
-//         <Route exact path="/" element={Home} />
-//         <Route path="/shop" component={Shop} />
-//         <Route path="/product" component={ProductDetail} />
-//         <Route path="/viewcart" component={ViewCart} />
-//         <Route path="/checkout" component={Checkout} />
-//         <Route path="/admin" component={AdminLogin} />
-//         <Route path="/dashboard" component={AdminHome} />
-//         <Route path="/add-product" component={AddProduct} />
-//         <Route path="/edit-product" component={EditProduct} />
-//         <Route path="/signup" component={UserRegistration} />
-//         <Route path="/login" component={UserLogin} />
- 
-//     </Router>
-//   );
-// }
-// export default BasicExample;
+export default Routing;
--- src/pages/UserSignup.jsx
+++ src/pages/UserSignup.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const UserSignup = () => {
   return (
     <div>
-      Sign Up
+      <form>
+        <label htmlFor='name'>Enter Your Name</label><br />
+        <input type='text' name='name' />
+        <br />
+        <label htmlFor='email'>Enter Your Email</label><br />
+        <input type='email' name='email' />
+        <br />
+        <label htmlFor='password'>Set Your Password</label><br />
+        <input type='text' name='password' />
+      </form>
     </div>
   )
 }
