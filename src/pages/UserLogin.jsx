c49a50907d73e687c9fa8461dea4a7adab0d2b89
ChiragSethi-1153 <chirag.1153@zenmonk.tech>
2024-01-24 13:37:01 +0530

    commit2

--- src/pages/UserLogin.jsx
+++ src/pages/UserLogin.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Button, bottomNavigationActionClasses} from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
 
 const UserLogin = () => {
+
+  const navigate = useNavigate();
+
+  const [password,setPassword]= useState("")
+  const [email,setEmail]= useState("") 
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const loggedUser = JSON.parse(localStorage.getItem("users"))
+    console.log(loggedUser)
+    let flag=false
+    loggedUser.map((item) => {
+        if(email === item.email && password === item.password){
+            flag= true
+        }
+    } )
+   flag?navigate("/"):alert("Invalid Credentails")
+  }
+
   return (
     <div>
-      Login
+      <form onSubmit={handleLogin} >
+        
+        <label htmlFor='email'>Enter Your Email</label><br />
+        <input type='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)}  />
+        <br />
+        <label htmlFor='password'>Enter Your Password</label><br />
+        <input type='text' name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+         <br /><br />
+         <Button variant="contained" type='submit'>Login</Button>
+     
+      </form>
     </div>
   )
 }
--- src/pages/UserSignup.jsx
+++ src/pages/UserSignup.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from '@mui/material'
+import { nanoid } from 'nanoid';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const UserSignup = () => {
+
+  const navigate = useNavigate();
+  
+
+
+  const [name,setName]= useState("")
+  const [password,setPassword]= useState("")
+  const [email,setEmail]= useState("")
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+          let users =  JSON.parse(localStorage.getItem("users"))
+          if(users===null){
+            users=[]
+            users?.push({name, email, password})
+          localStorage.setItem("users", JSON.stringify(users) )
+          }
+          else{
+          users?.push({name, email, password})
+          localStorage.setItem("users", JSON.stringify(users) )
+          }
+          navigate('/login');
+    } 
+ 
   return (
     <div>
-      <form>
+      <form  onSubmit={handleSubmit}>
         <label htmlFor='name'>Enter Your Name</label><br />
-        <input type='text' name='name' />
+        <input type='text' name='name' value={name} onChange={(e) => setName(e.target.value)} />
         <br />
         <label htmlFor='email'>Enter Your Email</label><br />
-        <input type='email' name='email' />
+        <input type='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)}  />
         <br />
         <label htmlFor='password'>Set Your Password</label><br />
-        <input type='text' name='password' />
+        <input type='text' name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+         <br /><br />
+         <Button variant="contained" type='submit'>Sign Up</Button>
+     
       </form>
     </div>
   )
